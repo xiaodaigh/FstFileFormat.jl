2cb499cfb70f0ff7889bec95d1a3618a6262cbc9
susabi <zhuojia.dai@gmail.com>
2017-10-23 23:04:31 +1100

    naive implementation via RCall

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fst
+# About
+This is the Julia bindings for the fst format (http://www.fstpackage.org) although the format was originally designed to work with R it can be language independent as well
--- src/fst.jl
+++ src/fst.jl
@@ -1,4 +1,35 @@
-module fst
+__precompile__(true)
+module fstformat
+
+using RCall
+
+function install_fst()
+  R"""
+    if(!require(fst)) {
+      install.packages("fst")
+    }
+  """
+end
+
+function read(path)
+  install_fst()
+  R"""
+    library(fst)
+    fst::read.fst($path)
+  """
+end
+
+function write(path)
+  write(path, 0)
+end
+
+function write(path, compress)
+  install_fst()
+  R"""
+    library(fst)
+    fst::read.fst($path,compress = $compress)
+  """
+end
 
 # package code goes here
 
