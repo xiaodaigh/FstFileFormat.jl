c5940269f1287a62f256326f1d5f3ba46034050e
David Anthoff <anthoff@berkeley.edu>
2019-08-04 11:46:00 -0700

    Update doc config

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,10 @@ using Documenter, FstFileFormat
 
 makedocs(modules=[FstFileFormat],
         doctest=true,
-        format = :html,
 	    sitename = "FstFileFormat.jl",
 	    pages = [
 		    "The FstFileFormat.jl package" => "index.md"])
 
-deploydocs(deps = nothing,
-    make = nothing,
-    target = "build",
-    repo = "github.com/xiaodaigh/FstFileFormat.jl.git",
-    julia  = "0.6")
+deploydocs(
+    repo = "github.com/xiaodaigh/FstFileFormat.jl.git"
+)
